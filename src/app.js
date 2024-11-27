390bc2217b787daafb6ee28bab9c6573ced6a8bf
LeandroMQ2018 <deinermamani34@gmail.com>
2024-11-27 18:53:06 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -13,18 +13,19 @@ dotenv.config();
 
 const app = express();
 
+// Configuración de CORS: permitir encabezados específicos y métodos
+app.use(cors({
+  origin: 'https://frontend1-25dt9fa3g-leandromq2018s-projects.vercel.app', // Solo permitir solicitudes de tu frontend
+  credentials: true, // Habilitar cookies o credenciales
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'], // Métodos permitidos
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin'], // Encabezados permitidos, incluyendo Content-Type
+  preflightContinue: false, // No continuar después de la respuesta de preflight
+}));
+
 // Middlewares
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cookieParser());
-
-// Configuración de CORS para permitir todo
-app.use(cors({
-  origin: true, // Permitir cualquier origen
-  credentials: true, // Permitir cookies o credenciales
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'], // Métodos permitidos
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin', 'X-Auth-Token'], // Especificar encabezados permitidos
-}));
 
 // Rutas
 app.use('/api/usuarios', usuarioRutas);
