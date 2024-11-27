216a8bcd77d9b51c3db496c27146099ec146e1ab
LeandroMQ2018 <deinermamani34@gmail.com>
2024-11-26 23:03:47 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -12,19 +12,29 @@ import notaRutas from './routes/nota.routes.js'; // Rutas de notas
 dotenv.config();
 
 const app = express();
-
+const allowedOrigins = [
+  'https://frontend1-alf8jfzql-leandromq2018s-projects.vercel.app', // Dominio actual de Vercel
+];
 // Middlewares
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cookieParser());
 // Configuración de CORS permitiendo cualquier origen
-app.use(cors({
-    origin: '*', // Cambia esto a tu URL de Vercel
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true, // Esto es necesario si usas autenticación basada en cookies
-}));
 
+
+// Configuración de CORS
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true); // Permitir el acceso
+    } else {
+      callback(new Error('No permitido por CORS'));
+    }
+  },
+  credentials: true, // Habilitar cookies y credenciales
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'], // Métodos permitidos
+  allowedHeaders: ['Content-Type', 'Authorization'], // Encabezados permitidos
+}));
 
 
 
